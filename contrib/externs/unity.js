fa058be63c7c044d62d224322717846beac0c3d0
Robbert <mail@robbertbroersma.nl>
2013-02-24 15:56:18 +0100

    Added extern for the Unity Web Player API.

---
+++ contrib/externs/unity.js
@@ -0,0 +1,46 @@
+/**
+ * @constructor
+ * @see http://docs.unity3d.com/Documentation/Manual/WorkingwithUnityObject.html
+ */
+function UnityObject2()
+{
+
+}
+
+/**
+ * @typedef {{ pluginStatus: string, targetEl: Element, bestMethod: string, unityObj: UnityObject2, ua: * }}
+ */
+UnityObject2.Progress;
+
+/**
+ * @param {function(UnityObject2.Progress)} callback
+ */
+UnityObject2.prototype.observeProgress = function (callback)
+{
+	//
+};
+
+/**
+ * @param {Element} containerElement
+ * @param {string} gameURL
+ */
+UnityObject2.prototype.initPlugin = function (containerElement, gameURL)
+{
+	//
+};
+
+/**
+ * @param {string=} method
+ */
+UnityObject2.prototype.installPlugin = function (method)
+{
+	//
+};
+
+/**
+ * @return {?Object}
+ */
+UnityObject2.prototype.getUnity = function ()
+{
+
+};
